9257867b5ea328547be358f7f6a93888cc5ea6fe
Kennylovetheworld <944545895@qq.com>
2019-11-23 13:00:24 -0500

    init

--- retrieval.py
+++ retrieval.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import pdb; pdb.set_trace()
+import pdb
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 def inverted_index(Gamedataframe):
     # accept pd.dataframe
@@ -10,10 +12,13 @@ def inverted_index(Gamedataframe):
 def static_rank(Gamedataframe):
     # accept pd.dataframe
     # return pd.dataframe with static_ranking
+    
     pass
 
 def main():
-    pd.read_excel("steam_modified.xlsx",index_col=0)
+    game_df = pd.read_excel("steam_modified.xlsx",index_col=0)
+    plt.plot(game_df['negative_ratings'].values)
+    plt.show()
 
 if __name__ == "__main__":
     main()
