6d0dd3b2e2712bf52d2d8b8172ba7e62b559dce2
Kennylovetheworld <944545895@qq.com>
2019-11-23 11:48:39 -0500

    new_clean_data

---
+++ retrieval.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pdb; pdb.set_trace()
+import numpy as np
+
+def inverted_index(Gamedataframe):
+    # accept pd.dataframe
+    # return an inverted index
+    pass
+
+def static_rank(Gamedataframe):
+    # accept pd.dataframe
+    # return pd.dataframe with static_ranking
+    pass
+
+def main():
+    pd.read_excel("steam_modified.xlsx",index_col=0)
+
+if __name__ == "__main__":
+    main()
