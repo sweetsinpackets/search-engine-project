94c1d3f90a4ef4c99a550e4566d6cbe201571383
杨书杰 <shujieyang@0587408914.wireless.umich.net>
2019-11-23 17:24:16 -0500

    Update BM25 ranking

--- clean.py
+++ clean.py
@@ -1,12 +1,106 @@
-import html2text
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import pandas as pd
 from bs4 import BeautifulSoup
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import scipy.sparse as sp
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+from sklearn.feature_extraction.text import _document_frequency
 
+'''
+https://github.com/arosh/BM25Transformer/blob/master/bm25.py
+'''
 
+class BM25Transformer(BaseEstimator, TransformerMixin):
+    """
+    Parameters
+    ----------
+    use_idf : boolean, optional (default=True)
+    k1 : float, optional (default=2.0)
+    b : float, optional (default=0.75)
+    References
+    ----------
+    Okapi BM25: a non-binary model - Introduction to Information Retrieval
+    http://nlp.stanford.edu/IR-book/html/htmledition/okapi-bm25-a-non-binary-model-1.html
+    """
+    def __init__(self, use_idf=True, k1=2.0, b=0.75):
+        self.use_idf = use_idf
+        self.k1 = k1
+        self.b = b
+
+    def fit(self, X):
+        """
+        Parameters
+        ----------
+        X : sparse matrix, [n_samples, n_features]
+            document-term matrix
+        """
+        if not sp.issparse(X):
+            X = sp.csc_matrix(X)
+        if self.use_idf:
+            n_samples, n_features = X.shape
+            df = _document_frequency(X)
+            idf = np.log((n_samples - df + 0.5) / (df + 0.5))
+            self._idf_diag = sp.spdiags(idf, diags=0, m=n_features, n=n_features)
+        return self
+
+    def transform(self, X, copy=True):
+        """
+        Parameters
+        ----------
+        X : sparse matrix, [n_samples, n_features]
+            document-term matrix
+        copy : boolean, optional (default=True)
+        """
+        if hasattr(X, 'dtype') and np.issubdtype(X.dtype, np.float):
+            # preserve float family dtype
+            X = sp.csr_matrix(X, copy=copy)
+        else:
+            # convert counts or binary occurrences to floats
+            X = sp.csr_matrix(X, dtype=np.float64, copy=copy)
+
+        n_samples, n_features = X.shape
+
+        # Document length (number of terms) in each row
+        # Shape is (n_samples, 1)
+        dl = X.sum(axis=1)
+        # Number of non-zero elements in each row
+        # Shape is (n_samples, )
+        sz = X.indptr[1:] - X.indptr[0:-1]
+        # In each row, repeat `dl` for `sz` times
+        # Shape is (sum(sz), )
+        # Example
+        # -------
+        # dl = [4, 5, 6]
+        # sz = [1, 2, 3]
+        # rep = [4, 5, 5, 6, 6, 6]
+        rep = np.repeat(np.asarray(dl), sz)
+        # Average document length
+        # Scalar value
+        avgdl = np.average(dl)
+        # Compute BM25 score only for non-zero elements
+        data = X.data * (self.k1 + 1) / (X.data + self.k1 * (1 - self.b + self.b * rep / avgdl))
+        X = sp.csr_matrix((data, X.indices, X.indptr), shape=X.shape)
+
+        if self.use_idf:
+            check_is_fitted(self, '_idf_diag', 'idf vector is not fitted')
+
+            expected_n_features = self._idf_diag.shape[0]
+            if n_features != expected_n_features:
+                raise ValueError("Input has n_features=%d while the model"
+                                 " has been trained with n_features=%d" % (
+                                     n_features, expected_n_features))
+            # *= doesn't work
+            X = X * self._idf_diag
+
+        return X
 
 def convert_html_to_text(html_list):
     return [BeautifulSoup(item).get_text() for item in html_list]
@@ -29,7 +123,26 @@ def description_preprocess(det_description_text):
 
     return filtered_sentence
 
+def dummy_tokenizer(text):
+    return text
 
+def get_count_mat(corpus):
+    vectorizer = CountVectorizer(tokenizer=dummy_tokenizer, lowercase=False)
+    X = vectorizer.fit_transform(corpus)
+    term_list = vectorizer.get_feature_names()
+    print(X.shape)
+    return X, term_list, vectorizer
+
+'''
+    input: tfidf-vectortizer and term
+    return: the column index of a word in the tfidf matrix
+'''
+def get_col_index(vectorizer, term):
+    # get index
+    return  vectorizer.vocabulary_.get(term)
+
+def query_proprocess(q):
+    return [w.lower() for w in q.split()]
 
 
 # Read data
@@ -43,9 +156,28 @@ det_description_text = convert_html_to_text(det_description_html)
 
 det_description_final = description_preprocess(det_description_text)
 
+#tfidf, col_name, vectorizer = get_tfidf(det_description_final)
 
-#games['detailed_description'] = h.handle(games['detailed_description'])
+#tfidf_as_array = tfidf.toarray()
 
-#print(h.handle("<p><strong>Zed's</strong> dead baby, <em>Zed's</em> dead.</p>"))
+count_mat, col_name, vectorizer = get_count_mat(det_description_final)
 
-print('1')
+BM25_vec = BM25Transformer()
+
+BM25_vec = BM25_vec.fit(count_mat)
+
+BM25_mat = BM25_vec.transform(count_mat)
+
+BM25_as_array = BM25_mat.toarray()
+
+query = query_proprocess('ancient egypt')
+
+index_list = [get_col_index(vectorizer, w) for w in query]
+
+score = np.sum(BM25_as_array[:,index_list], axis=1)
+
+BM25_tuples = list(zip(games['name'], score))
+
+BM25_tuples.sort(key=lambda tup: tup[1], reverse=True)
+
+print(BM25_tuples[:20])
